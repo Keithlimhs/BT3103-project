329f506de1d24de7c205a16ff5057c9afdbe9752
Keithlimhs <hslim03@hotmail.com>
2022-03-21 21:29:11 +0800

    Top header

--- src/components/DropDownMenu.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda maxime sit facilis nulla quis at totam enim vitae, libero id quisquam odio nam iusto odit in deserunt atque commodi. Voluptatum.
-    </div>
-</template>
