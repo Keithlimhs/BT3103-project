ea7b8381d2cd98e4a16eb8e1a69a70c01a4076a1
edwin716 <65332564+edwin716@users.noreply.github.com>
2022-03-21 22:53:07 +0800

    added tutor home page and tutors list page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
-
+<template>
+  <TutorsList/>
+  <TutorHome/>
+</template>
 
 <script>
-
+import TutorsList from './views/TutorsList.vue'
+import TutorHome from './views/TutorHome.vue'
 
 export default {
   name: 'App',
   components: {
-
+    TutorsList,
+    TutorHome,
   }
 }
 </script>
@@ -17,7 +22,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+  background-color: #E5E5E5;
 }
 </style>
---
+++ src/views/TutorHome.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="tutor-home">
+    <div class="top-container">
+      Hello {{user}}, here are your offers for today
+    </div>
+    <div class="bottom-container">
+      <div class="ongoing">
+        <div class="header">
+          Ongoing
+        </div>
+        <TuteeProfileDisplay/>
+      </div>
+      <div class="accepted">
+        <div class="header">
+          Accepted
+        </div>
+        <TuteeProfileDisplay/>
+      </div>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+import TuteeProfileDisplay from '../components/TuteeProfileDisplay.vue'
+
+export default {
+  name: 'TutorHome',
+  components: {
+      TuteeProfileDisplay,
+  },
+  data(){
+    return{
+      user: "Shashank Shekhar Tripathi",
+    }
+  },
+}
+</script>
+
+<style scoped>
+.tutor-home {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+.top-container {
+  text-align: left;
+  font-size: 2vw;
+  font-weight: bolder;
+  padding: 0px 0px 10px 10px;
+}
+
+.bottom-container {
+	display: flex;
+	flex-direction: row;
+  font-size: 1vw;
+}
+
+.ongoing, .accepted {
+  border: 2px solid black;
+  width: 50%;
+  height: 100vh;
+}
+
+.header {
+  font-weight: 600;
+  font-size: 2vw;
+  border-bottom: 2px solid black;
+  padding: 15px;
+}
+</style>
