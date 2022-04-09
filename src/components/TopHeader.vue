773ac0cd2b6aff63c802b5ebcd9d09d8fa510a6d
jettyevy <siejiesiang@gmail.com>
2022-04-10 01:35:56 +0800

    dropdown menu component added to TopHeader and HomeHeader

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -2,31 +2,8 @@
     <div id = "header">
 
         <a href = "history.back()" class = "previous"><img id = "back" v-bind:src="require('@/assets/backbutton.png')" alt="error"></a>
-        
-
-        <div class="dropdown">
-                <img id = "profilePic" v-bind:src = "require('@/assets/logo.png')" alt = "error">
-                <button class="dropbtn">
-                    <i class="fa fa-caret-down"></i>
-                </button>
-                <div class="dropdown-content">
-                    <a href="#"> 
-                        <img id = "home_icon" v-bind:src = "require('@/assets/home_icon.png')" alt = "error" width = 20>
-                        Home</a>
-                    <a href="#"> 
-                        <img id = "chat_icon" v-bind:src = "require('@/assets/chat_icon.png')" alt = "error" width = 20>
-                        Chat</a>
-                    <a href="#">
-                        <img id = "settings_icon" v-bind:src = "require('@/assets/settings_icon.png')" alt = "error" width = 20>
-                        Settings</a>
-                    <a href="#">
-                        <img id = "switch_account_icon" v-bind:src = "require('@/assets/switch_account_icon.png')" alt = "error" width = 20>
-                        Switch Accounts</a>
-                    <a href="#">
-                        <img id = "logout_icon" v-bind:src = "require('@/assets/logout_icon.png')" alt = "error" width = 20>
-                        Log Out</a>
-                </div>
-            </div>
+        <img id = "profilePic" v-bind:src = "require('@/assets/logo.png')" alt = "error">
+        <DropdownMenu/>
 
 
         <h2 id = "title">{{ pageName }}</h2>
@@ -34,14 +11,18 @@
 </template>
 
 <script>
+import DropdownMenu from '../components/DropdownMenu.vue'
+
 export default {
 
 
     props: {
             pageName: String
 
+    },
+    components: {
+        DropdownMenu,
     }
-
     
 }
 </script>
@@ -78,51 +59,4 @@ img {
     width: 30px;
 }
 
-
-
-
-
-.dropdown {
-  float: right;
-  overflow: hidden;
-  display:inline;
-  background-color: white;
-}
-
-.dropdown .dropbtn {
-  font-size: 16px;  
-  border: none;
-  outline: none;
-  color: white;
-  padding: 14px 16px;
-  background-color: inherit;
-  font-family: inherit;
-  margin: 0;
-  margin-right: 5vw;
-}
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-}
-
-.dropdown-content a {
-  float: none;
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-  text-align: left;
-}
-
-.dropdown-content a:hover {
-  background-color: #ddd;
-}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-}
 </style>
